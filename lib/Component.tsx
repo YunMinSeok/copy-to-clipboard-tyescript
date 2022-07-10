e06ee174504c2a2d4569a1a40713028d53d35738
윤민석 <ykmvm147@naver.com>
2022-07-10 17:09:06 +0900

    [Feat] First demp clipboard compoentns

--- lib/Component.tsx
+++ lib/Component.tsx
@@ -1,5 +1,38 @@
-import { PureComponent } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import copy from "copy-to-clipboard";
 
-export class ClipBoard extends PureComponent {}
+interface Props {
+  onCopy: Function;
+  children: JSX.Element;
+  text: string;
+}
+export class ClipBoard extends React.Component<Props> {
+  static defaultProps: Props = {
+    onCopy: undefined,
+    children: undefined,
+    text: "",
+  };
+
+  onClick = (event) => {
+    const { text, onCopy, children } = this.props;
+
+    const elem = React.Children.only(children);
+
+    const result = copy(text);
+
+    if (onCopy) {
+      onCopy(text, result);
+    }
+
+    if (elem && elem.props && typeof elem.props.onClick === "function") {
+      elem.props.onClick(event);
+    }
+  };
+
+  render() {
+    const { text: _text, onCopy: _onCopy, children, ...props } = this.props;
+    const elem = React.Children.only(children);
+
+    return React.cloneElement(elem, { ...props, onClick: this.onClick });
+  }
+}
