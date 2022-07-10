8027e1b09109df5bdff010b8f769f3892a35a401
윤민석 <ykmvm147@naver.com>
2022-07-10 16:02:34 +0900

    [Feat] Update dependencies library && Create Component

---
+++ lib/Component.tsx
@@ -0,0 +1,5 @@
+import { PureComponent } from "react";
+import PropTypes from "prop-types";
+import copy from "copy-to-clipboard";
+
+export class ClipBoard extends PureComponent {}
---
+++ lib/index.ts
@@ -0,0 +1,1 @@
+export { ClipBoard } from "./Component";
