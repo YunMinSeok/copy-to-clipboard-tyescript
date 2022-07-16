bad93510833243f377a26390c487f6f012239abe
윤민석 <ykmvm147@naver.com>
2022-07-16 18:40:18 +0900

    [Feat] Update code line

--- lib/Component.tsx
+++ lib/Component.tsx
@@ -12,11 +12,10 @@ export class ClipBoard extends React.Component<Props> {
     children: undefined,
     text: "",
   };
-  //  TODO :: 코드 리팩토링 및 재배포 해야함 테스트도!!
   onClick = (event) => {
     const { text, onCopy, children } = this.props;
 
-    const elem = React.Children.only(children);
+    const element = React.Children.only(children);
 
     const result = copy(text);
 
@@ -24,15 +23,19 @@ export class ClipBoard extends React.Component<Props> {
       onCopy(text, result);
     }
 
-    if (elem && elem.props && typeof elem.props.onClick === "function") {
-      elem.props.onClick(event);
+    if (
+      element &&
+      element.props &&
+      typeof element.props.onClick === "function"
+    ) {
+      element.props.onClick(event);
     }
   };
 
   render() {
     const { text: _text, onCopy: _onCopy, children, ...props } = this.props;
-    const elem = React.Children.only(children);
+    const element = React.Children.only(children);
 
-    return React.cloneElement(elem, { ...props, onClick: this.onClick });
+    return React.cloneElement(element, { ...props, onClick: this.onClick });
   }
 }
